92ec7304d9783a47d7f9dd2df5017aa175413fc4
Juliana <julianamq20@gmail.com>
2023-01-27 16:22:01 -0300

    passou

--- app/backend/src/MatchesRouter.ts
+++ app/backend/src/MatchesRouter.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express';
 import MatchesController from './controller/MatchesController';
-import { validateMatchesData } from './middelwares/Matche';
+import { validateEqual, validateMatchesData } from './middelwares/Matche';
 // import Matche from './middelwares/Matche';
 import { validateToken } from './middelwares/token';
 
 const router = Router();
 router.get('/', MatchesController.getAllMatches);
 router.get('/?', MatchesController.getMatchByQuery);
-router.post('/', validateToken, validateMatchesData, MatchesController.createMatches);
+router.post(
+  '/',
+  validateToken,
+  validateEqual,
+  validateMatchesData,
+  MatchesController.createMatches,
+);
 router.patch('/:id/finish', MatchesController.getFinish);
 router.patch('/:id', MatchesController.byUpdate);
 export default router;
